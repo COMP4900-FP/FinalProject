d194351a352fc7354d9e497e914a73f85e05fa81
Cara Swab <bawstc@gmail.com>
2022-03-27 18:50:09 -0400

    defining some structs we can add to/build off of

--- src/server.c
+++ src/server.c
@@ -2,6 +2,19 @@
 #include <stdlib.h>
 #include "server.h"
 #include <unistd.h>
+
+
+// messages will be received as a receive_t
+typedef union {
+	uint16_t type;
+	struct _pulse pulse;
+	distribute_water_t distribute_water;
+	check_humidity_t check_humidity;
+	check_temperature_t check_temperature;
+	change_light_t change_light;
+} receive_t;
+
+
 int main(void) {
 
 	puts("Done!");
--- src/server.h
+++ src/server.h
@@ -1,3 +1,43 @@
 #include <stdint.h>
 #include <sys/iomsg.h>
 #include <sys/mman.h>
+
+
+// water distribution
+typedef struct distribute_water {
+	uint16_t type;
+} distribute_water_t;
+
+typedef struct distribute_water_resp {
+	int placeholder;
+} distribute_water_resp_t;
+
+
+// humidity
+typedef struct check_humidity {
+	uint16_t type;
+} check_humidity_t;
+
+typedef struct check_humidity_resp {
+	int placeholder;
+} check_humidity_resp_t;
+
+
+// temperature
+typedef struct check_temperature {
+	uint16_t type;
+} check_temperature_t;
+
+typedef struct check_temperature_resp {
+	int placeholder;
+} check_temperature_resp_t;
+
+
+// light
+typedef struct change_light {
+	uint16_t type;
+} change_light_t;
+
+typedef struct change_light_resp {
+	int placeholder;
+} change_light_resp_t;
