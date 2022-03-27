73dc4d441b6a9f05a041ad98910b2b9e05378525
Clark Bains <clarkbains@gmail.com>
2022-03-27 12:21:33 -0400

    setup make
    Signed-off-by: Clark Bains <clarkbains@gmail.com>

---
+++ src/client.c
@@ -0,0 +1,15 @@
+/*
+ * ClientLauncher.c
+ *
+ *  Created on: Mar 27, 2022
+ *      Author: cbains
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "server.h"
+
+int main(void) {
+	puts("Hello Client!!!"); /* prints Hello World!!! */
+	return EXIT_SUCCESS;
+}
--- src/server.c
+++ src/server.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "server.h"
 int main(void) {
 	puts("Hello World!!!"); /* prints Hello World!!! */
 	return EXIT_SUCCESS;
