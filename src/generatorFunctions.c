35886119bff2a4fbf7b38c25016623609798d2be
Hayden Mirza <haydenjwmirza@gmail.com>
2022-03-28 20:16:06 -0400

    init files

---
+++ src/generatorFunctions.c
@@ -0,0 +1,8 @@
+/*
+ * generatorFunctions.c
+ *
+ *  Created on: Mar. 28, 2022
+ *      Author: Hayden
+ */
+
+
---
+++ src/generatorFunctions.h
@@ -0,0 +1,13 @@
+/*
+ * generatorFunctions.h
+ *
+ *  Created on: Mar. 28, 2022
+ *      Author: Hayden
+ */
+
+#ifndef SRC_GENERATORFUNCTIONS_H_
+#define SRC_GENERATORFUNCTIONS_H_
+
+
+
+#endif /* SRC_GENERATORFUNCTIONS_H_ */
